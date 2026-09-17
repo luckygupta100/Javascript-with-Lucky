605a13f9bf0408681874d335d40cd90f3ad2a0d1
Lucky Gupta <lucky100gpt@gmail.com>
2026-09-17 07:48:12 +0530

    switch cases 2

--- ifelse_04.js
+++ ifelse_04.js
@@ -15,15 +15,28 @@ age>= 18 ? console.log('Yes') : console.log('No')
 // cond|ternary operator| true cond |false cond
 
 // switch cases
-const option =2
+// const option =2
 
-switch(option){
-    case 1 :
-        console.log("Namste")
-        break;
-    case 2 :
-        console.log("bonjour")
-        break;
-    default:console.log("noo")
+// switch(option){
+//     case 1 :
+//         console.log("Namste")
+//         break;
+//     case 2 :
+//         console.log("bonjour")
+//         break;
+//     default:console.log("noo")
     
+// }
+
+let a=30;
+let opt ='*';
+let b=40;
+
+switch(opt){
+    case '+' : console.log(a+b);
+    break;
+    case '-' : console.log(b-a);
+    case '*' : console.log(a*b);
+    break;
+    default : console.log('IDK')
 }
