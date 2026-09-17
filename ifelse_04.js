f06df253fdcddd07d6c5175716bc78276e7984db
Lucky Gupta <lucky100gpt@gmail.com>
2026-09-17 07:44:51 +0530

    switch cases 1

--- ifelse_04.js
+++ ifelse_04.js
@@ -1,10 +1,29 @@
-const age =19;
-if(age >= 18){
-    console.log("yes,you can vote")
-}
-else if (age>= 80){
-    console.log("you are adult")
-}
-else{
-    console.log("you cannot vote")
+// const age =19;
+// if(age >= 18){
+//     console.log("yes,you can vote")
+// }
+// else if (age>= 80){
+//     console.log("you are adult")
+// }
+// else{
+//     console.log("you cannot vote")
+// }
+
+// Ternary Operator
+const age =90;
+age>= 18 ? console.log('Yes') : console.log('No')
+// cond|ternary operator| true cond |false cond
+
+// switch cases
+const option =2
+
+switch(option){
+    case 1 :
+        console.log("Namste")
+        break;
+    case 2 :
+        console.log("bonjour")
+        break;
+    default:console.log("noo")
+    
 }
