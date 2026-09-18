81b750fddffc35aa08e6c6d7ffcbbd7f9b4a4b37
Lucky Gupta <lucky100gpt@gmail.com>
2026-09-18 21:38:27 +0530

    logicaloperator

---
+++ logicaloperator.js
@@ -0,0 +1,16 @@
+// Logical Operator
+// AND-->all condition must be true.
+
+const age =20;
+const gender = 'Male';
+
+if(age >= 18 && gender == 'Male'){
+    console.log('You are adult male')
+}
+// OR --> at least one condition true 
+const ag =20;
+const gende = 'Male';
+
+if(ag >= 18 | gende  == 'Male'){
+    console.log('You are adult male')
+}
