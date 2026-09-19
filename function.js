896f4d73f5be4ebc8d2797df5eefc30d665e33ea
Lucky Gupta <lucky100gpt@gmail.com>
2026-09-19 17:59:33 +0530

    functions

---
+++ function.js
@@ -0,0 +1,20 @@
+// functions 
+
+function sayNamaste(){
+    console.log("Namaste")
+}
+sayNamaste()
+
+// Parameters
+function add(a,b){
+    console.log(a+b)
+}
+
+add(500,565)
+
+function multiply(a,b){
+    return  a*b
+}
+
+a = multiply(9,45)
+console.log("Dear sir, the multiply of these two numbers will be",a)
