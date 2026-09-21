f311b9e723a0aa278b47a8f69ec6dcff6cb57bef
Lucky Gupta <lucky100gpt@gmail.com>
2026-09-21 07:35:26 +0530

    Unlimited Arguments Accept.

--- function.js
+++ function.js
@@ -18,3 +18,14 @@ function multiply(a,b){
 
 a = multiply(9,45)
 console.log("Dear sir, the multiply of these two numbers will be",a)
+
+// Unlimited Arguments Accept.
+function addNumbers (){
+    let ans = 0
+    for(i=0;i<arguments.length;i++){
+        ans=ans+arguments[i]
+    }
+    return ans
+}
+let result =addNumbers(565,89,99,88,77,66,55,44,33,22,11)
+console.log(result)
